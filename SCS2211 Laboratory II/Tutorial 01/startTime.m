43815a246429c366ad967e8883a8b636426e0f17
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2020-12-18 23:11:01 +0530

    Add files via upload

---
+++ SCS2211 Laboratory II/Tutorial 01/startTime.m
@@ -0,0 +1,15 @@
+# Created by Octave 5.2.0, Fri Oct 23 18:55:28 2020 GMT <unknown@AsusPC>
+# name: start
+# type: matrix
+# rows: 1
+# columns: 6
+ 2020 10 23 16 45 27.785768999999998
+
+
+# name: startString
+# type: sq_string
+# elements: 1
+# length: 20
+23-Oct-2020 16:45:27
+
+
