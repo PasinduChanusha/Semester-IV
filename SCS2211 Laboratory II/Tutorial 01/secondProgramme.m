ec7e92c3bb0860d2ade58271ca1a35f1c6e5e339
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2020-12-18 23:10:37 +0530

    Add files via upload

---
+++ SCS2211 Laboratory II/Tutorial 01/secondProgramme.m
@@ -0,0 +1,12 @@
+A =  105
+B =  9832.5
+C =  7 + 5i
+D =  4 - 2i
+E =  3 + 7i
+
+aVec = [5.3 3.4 6 23]
+bVec = [100 6 5.3 3.5]
+cVec = [5 4.5 4 3.5 3 2.5 2 1.5 1 0.5 0 -0.5 -1 -1.5 -2 -2.5 -3 -3.5 -4 -4.5 -5]
+dVec = "University of Colombo School of Computing"
+
+
