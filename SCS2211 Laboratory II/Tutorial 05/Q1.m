e6ef679bdea4fedd08947dbe7b2e02b463db7bec
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2021-01-15 18:35:12 +0530

    Add files via upload

---
+++ SCS2211 Laboratory II/Tutorial 05/Q1.m
@@ -0,0 +1,14 @@
+a = [3;5;7;];
+b = [1;2;3;];
+
+a1=dot(a,b);
+printf("dot product, result is equivalent = %d \n",a1);
+
+a2=cross(a,b);
+
+printf("cross product of vector a and vector b=");
+display(a2);
+
+b1=norm(b)
+printf("projection of a onto ");
+pab = (a1/(b1)^2)*b
