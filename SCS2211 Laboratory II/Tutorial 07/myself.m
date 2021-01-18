ecb20a5d49ff40d19eeaa75bcb907abca50ff33a
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2021-01-18 23:18:31 +0530

    Create myself.m

---
+++ SCS2211 Laboratory II/Tutorial 07/myself.m
@@ -0,0 +1,8 @@
+name="Pasindu";
+index=18001645;
+bornEven="true";
+height=1.76;
+printf("%50s My name is %s.\n",'',name);
+printf("%50s Index number is %d.\n",'',index);
+printf("%50s I was born in even day %s.\n",'',bornEven);
+printf("%50s Height is %fm.\n",'',height);
