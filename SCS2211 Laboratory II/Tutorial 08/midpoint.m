3ee9e61bbf230be5418832bfc551d12b4a715351
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2021-01-20 23:42:29 +0530

    Add files via upload

---
+++ SCS2211 Laboratory II/Tutorial 08/midpoint.m
@@ -0,0 +1,11 @@
+function result=midpoint(funct,a,b,n)
+  dx=abs(b-a)/n;
+  result=0;
+  for(k=0:n-1)
+     result=result+funct(a+(k*dx));-1,1,
+  endfor
+  result=result*dx;
+endfunction
+
+#f=@(x) x*x + 2*x -1
+#midpoint(f,-1,1,10)
